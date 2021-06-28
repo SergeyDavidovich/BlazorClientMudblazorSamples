2d0b21aaf88c75239b36e8382b8aa9866ed37955
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-06-28 14:14:34 +0200

    no comments

--- BlazorClientMudblazorSamples/Client/Program.cs
+++ BlazorClientMudblazorSamples/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorClientMudblazorSamples.Client.Services.BrowserService;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,6 +22,7 @@ namespace BlazorClientMudblazorSamples.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+            builder.Services.AddSingleton<BrowserService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorClientMudblazorSamples/Client/Services/BrowserService/BrowserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.JSInterop;
+
+namespace BlazorClientMudblazorSamples.Client.Services.BrowserService
+{
+    public class BrowserService
+    {
+        private readonly IJSRuntime _js;
+
+        public BrowserService(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task<BrowserDimension> GetDimensions()
+        {
+            var result= await _js.InvokeAsync<BrowserDimension>("getDimensions");
+            return result;
+        }
+
+    }
+    public class BrowserDimension
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
